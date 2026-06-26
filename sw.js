7989e7e69ff807fb1050e5be1f8967f54a48a7ed
apisabanacol <apisabana.col@gmail.com>
2026-06-26 10:15:20 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'apivoz-v1';
+const ARCHIVOS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) { return c.addAll(ARCHIVOS); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).catch(function() { return caches.match('/index.html'); });
+    })
+  );
+});
